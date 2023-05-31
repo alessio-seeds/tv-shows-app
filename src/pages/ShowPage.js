f0d43a54b1647fa41d8d46e64cede95bc603865d
ikethepike <ikethepike@outlook.com>
2023-05-31 18:08:46 +0200

    I didn't push it

--- src/pages/ShowPage.js
+++ src/pages/ShowPage.js
@@ -1,35 +1,46 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import { getShowByID } from "../helpers/showsHelper"
-import NotFound from "./NotFound"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getShowByID } from "../helpers/showsHelper";
+import NotFound from "./NotFound";
 
 function ShowPage() {
-  const {showID} = useParams()
-  const [show,setShow] = useState(undefined) 
+  const { showID } = useParams();
+  const [show, setShow] = useState(undefined);
 
-  useEffect( (() => {
+  useEffect(() => {
     const fetchShow = async () => {
-      const showData = await getShowByID(showID)
-      console.log(showData)
-      setShow(showData)
-    }
-    fetchShow()
-  }), [showID])
+      const showData = await getShowByID(showID);
+      console.log(showData);
+      setShow(showData);
+    };
+    fetchShow();
+  }, [showID]);
 
-  if(!show) return <NotFound />
+  if (!show) return <NotFound />;
+
+  const addFavorite = () => {
+    const existingFavorites = JSON.parse(
+      localStorage.getItem("favorites") || "[]"
+    );
+
+    const favorites = JSON.stringify([showID, ...existingFavorites]);
+    localStorage.setItem("favorites", favorites);
+  };
 
   return (
-        <div className="ShowPage">
-          <h1>{show.name}</h1>
-          <img src={show.image.medium} />
-          <h2>Language: {show.language}</h2>
-          <h3>Rating: {show.rating.average}</h3>
-          <h3>Genres: {show.genres.join(', ')}</h3>
-          {!!show._embedded.cast && show._embedded.cast.map((member)=><div>{member.person.name + ' as ' + member.character.name}</div>)}
-        </div>
- 
-  )
+    <div className="ShowPage">
+      <h1>{show.name}</h1>
+      <img src={show.image.medium} />
+      <h2>Language: {show.language}</h2>
+      <h3>Rating: {show.rating.average}</h3>
+      <h3>Genres: {show.genres.join(", ")}</h3>
+      {!!show._embedded.cast &&
+        show._embedded.cast.map((member) => (
+          <div>{member.person.name + " as " + member.character.name}</div>
+        ))}
+
+      <button onClick={addFavorite}>Add to favorites</button>
+    </div>
+  );
 }
-export default ShowPage
-
-
+export default ShowPage;
